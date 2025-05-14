6497d9a272e50b499882035a81318127052755a7
karlos marques de menezes <103792595+karlosmarques@users.noreply.github.com>
2025-05-14 12:21:08 -0300

    update

--- routes/publica/publica.js
+++ routes/publica/publica.js
@@ -2,30 +2,53 @@ import express from "express";
 import { PrismaClient } from '@prisma/client';
 import bcrypt from "bcrypt";
 
-const prisma = new PrismaClient();
+
 const router = express.Router();
+const prisma = new PrismaClient();
+
 
 // cadastro
 router.post("/registro", async (req, res) => {
   try {
-    const user = req.body;
+    const Registro = req.body;
 
     const salt = await bcrypt.genSalt(10);
-    const hashsenha = await bcrypt.hash(user.senha, salt);
+    const hashsenha = await bcrypt.hash(Registro.senha, salt);
 
-    await prisma.registro.create({
+   const sla =  await prisma.Registro.create({
       data: {
-        nome: user.nome,
-        email: user.email,
+        nome: Registro.nome,
+        email: Registro.email,
         senha: hashsenha,
-        datanascimento: user.datanascimento,
+        datanascimento: new Date(Registro.datanascimento)
       }
-    });
+    })
 
-    res.status(201).json(user);
+
+
+    res.status(201).json(sla);
   } catch (error) {
+    console.error("Erro ao criar o usuário:", error);
     res.status(500).json({ error: "Erro ao criar o usuário" });
   }
 });
+
+router.post ("/login", async (req, res) => {
+  try{
+    login = req.body;
+
+    const usuario = await prisma.Registro.findUnique({
+      where: {
+        email: login.email
+
+      }
+    })
+  
+  }catch (error) {
+    console.error("Erro ao fazer login:", error);
+    res.status(500).json({ error: "Erro ao fazer login" });
+    
+  }
+
 
 export default router;
